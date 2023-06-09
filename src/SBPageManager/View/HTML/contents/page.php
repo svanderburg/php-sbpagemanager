39619e04086173660844c91bd7a00aba4d9af985
Sander van der Burg <svanderburg@gmail.com>
2023-06-09 18:04:34 +0200

    Fix structuring error

--- src/SBPageManager/View/HTML/contents/page.php
+++ src/SBPageManager/View/HTML/contents/page.php
@@ -3,17 +3,12 @@ global $currentPage, $crudInterface;
 
 if($currentPage->checker->checkWritePermissions())
 {
-	?>
-	<p>
-		<?php
-		if(array_key_exists("query", $GLOBALS) && count($GLOBALS["query"]) > 0)
-		{
-			global $route;
-			\SBCrud\View\HTML\displayOperationToolbar($route, count($route->ids) - 1);
-		}
-		?>
-	</p>
-	<?php
+	if(array_key_exists("query", $GLOBALS) && count($GLOBALS["query"]) > 0)
+	{
+		global $route;
+		\SBCrud\View\HTML\displayOperationToolbar($route, count($route->ids) - 1);
+	}
+
 	\SBData\View\HTML\displayEditableForm($crudInterface->form);
 	?>
 	<script type="text/javascript">
